8d9c2f884b0c6e7e6705e1251c88a16262169544
sanda <sandasomlenze1997@gmail.com>
2023-03-28 14:22:57 +0200

    FIRST COMMIT

---
+++ Challenge 3/script3.js
@@ -0,0 +1,11 @@
+/*
+It is important to show the following message in order to:
+- to scare hackers
+- to impress investors
+*/
+
+console.warn("Security scan starting")
+
+// It is important to let user know when they can close the page  
+
+console.info('Please wait for scan to complete before closing the browser.');
